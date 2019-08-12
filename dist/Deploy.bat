f06133669be566b0afdd2bae048cce228bc07c54
Rolf Bänziger <rolf@perplex.world>
2019-08-12 10:54:03 +0100

    Adding distribution file.

--- dist/Deploy.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Run this to deploy andy web files and templates
-rem Place this script in the root folder the portal configuration, 
-rem along with the PortalDeployer.exe and its dependencies
-
-PortalDeployer deploy --directory  %~dp0
